4a5b5410bcceb7405cbf744f58d63d15648f4ed4
jubaer145 <mdjubaerhossain7@gmail.com>
2024-05-23 23:41:58 +0600

    Add files for hypertensive disorders of pregnancy

---
+++ protocols/hypertensive-disorders-pregnancy/README.md
@@ -0,0 +1,20 @@
+# Screening for Hypertensive Disorders of Pregnancy
+
+## Purpose of the protocol
+To update the US Preventive Services Task Force (USPSTF) recommendations on screening for hypertensive disorders of pregnancy to improve maternal and fetal health outcomes.
+
+## Target population
+- if pregnant
+- if unknown chronic hypertension status or known chronic hypertension
+- if gestational age >= 20 weeks
+
+## Recommendation
+The USPSTF recommends screening with blood pressure measurements throughout pregnancy to detect hypertensive disorders of pregnancy early and manage them effectively.
+
+## Importance
+- Prevalence: Hypertensive disorders of pregnancy are among the leading causes of maternal morbidity and mortality in the US.
+- Health Risks: Serious maternal morbidities include cerebrovascular accidents, retinal detachment, organ damage or failure, and eclamptic seizures. Adverse perinatal outcomes include intrauterine growth restriction, low birth weight, and stillbirth.
+- Disparities: Black individuals have higher rates of hypertensive disorders of pregnancy and preeclampsia-related complications compared to White individuals.
+
+## Conclusion
+There is moderate certainty that screening for hypertensive disorders of pregnancy with blood pressure measurements provides a substantial net benefit by reducing maternal and perinatal morbidity and mortality. The harms of screening and intervention are minimal.
---
+++ protocols/hypertensive-disorders-pregnancy/protocol.py
@@ -0,0 +1,178 @@
+from canvas_workflow_kit.value_set import ValueSet
+from canvas_workflow_kit.patient import Patient
+from typing import Type
+from typing import List
+from canvas_workflow_kit.recommendation import AllergyRecommendation
+from canvas_workflow_kit.recommendation import AssessRecommendation
+from canvas_workflow_kit.recommendation import DiagnoseRecommendation
+from canvas_workflow_kit.recommendation import FollowUpRecommendation
+from canvas_workflow_kit.recommendation import ImagingRecommendation
+from canvas_workflow_kit.recommendation import ImmunizationRecommendation
+from canvas_workflow_kit.recommendation import InstructionRecommendation
+from canvas_workflow_kit.recommendation import InterviewRecommendation
+from canvas_workflow_kit.recommendation import LabRecommendation
+from canvas_workflow_kit.recommendation import PerformRecommendation
+from canvas_workflow_kit.recommendation import PlanRecommendation
+from canvas_workflow_kit.recommendation import PrescribeRecommendation
+from canvas_workflow_kit.recommendation import Recommendation
+from canvas_workflow_kit.recommendation import ReferRecommendation
+from canvas_workflow_kit.recommendation import StructuredAssessmentRecommendation
+from canvas_workflow_kit.recommendation import TaskRecommendation
+from canvas_workflow_kit.recommendation import VitalSignRecommendation
+import arrow
+from canvas_workflow_kit.constants import CHANGE_TYPE
+from canvas_workflow_kit.protocol import ClinicalQualityMeasure
+from canvas_workflow_kit.protocol import STATUS_DUE
+from canvas_workflow_kit.protocol import STATUS_SATISFIED
+from canvas_workflow_kit.protocol import ProtocolResult
+from canvas_workflow_kit.timeframe import Timeframe
+class PregnantPersons(ValueSet):
+    VALUE_SET_NAME = 'Pregnant Persons'
+    SNOMEDCT = {'77386006'}
+
+class WithoutHypertensiveDisordersOfPregnancy(ValueSet):
+    VALUE_SET_NAME = 'Without Hypertensive Disorders of Pregnancy'
+    ICD10CM = {'O10', 'O11', 'O13', 'O14', 'O15'}
+    SNOMEDCT = {'46177005', '23724001', '398254007', '64217002'}
+
+class WithoutChronicHypertension(ValueSet):
+    VALUE_SET_NAME = 'Without Chronic Hypertension'
+    ICD10CM = {'I10'}
+    SNOMEDCT = {'38341003', '59621000'}
+
+def in_denominator(patient: Patient) -> bool:
+    pregnant = patient.conditions.find(PregnantPersons)
+    if not pregnant:
+        return False
+    hypertensive_disorders = patient.conditions.find(WithoutHypertensiveDisordersOfPregnancy)
+    if hypertensive_disorders:
+        return False
+    chronic_hypertension = patient.conditions.find(WithoutChronicHypertension)
+    if chronic_hypertension:
+        return False
+    return True
+def in_numerator(patient: Patient) -> bool:
+    return False
+class ComposerResult:
+
+    def __init__(self, patient: Patient) -> None:
+        self.patient = patient
+        self.recommendations: List[Recommendation] = []
+
+    def add_task(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_TASK_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(TaskRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+    def add_allergy_assessment(self, title: str, description: str, potential_allergy: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_ALLERGY_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(AllergyRecommendation(key=key, allergy=potential_allergy, title=title, narrative=description))
+
+    def add_condition_assessment(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_ASSESSMENT_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(AssessRecommendation(key=key, title=title, context={'narrative': description}, patient=self.patient))
+
+    def add_condition_diagnosis(self, title: str, description: str, condition_to_diagnose: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_DIAGNOSIS_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(DiagnoseRecommendation(key=key, condition=condition_to_diagnose, title=title, narrative=description, patient=self.patient))
+
+    def add_follow_up_appointment(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_FOLLOW_UP_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(FollowUpRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+    def add_imaging_order(self, title: str, description: str, imaging_to_order: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_IMAGING_ORDER_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(ImagingRecommendation(key=key, imaging=imaging_to_order, title=title, narrative=description, patient=self.patient))
+
+    def add_immunization_administration(self, title: str, description: str, immunization_to_administer: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_IMMUNIZATION_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(ImmunizationRecommendation(key=key, immunization=immunization_to_administer, title=title, narrative=description, patient=self.patient))
+
+    def add_instruction(self, title: str, description: str, instruction_to_give: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_INSTRUCTION_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(InstructionRecommendation(key=key, instruction=instruction_to_give, title=title, narrative=description, patient=self.patient))
+
+    def add_interview(self, title: str, description: str, questionnaire_to_complete: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_INTERVIEW_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(InterviewRecommendation(key=key, questionnaires=[questionnaire_to_complete], title=title, narrative=description, patient=self.patient))
+
+    def add_lab_order(self, title: str, description: str, laboratory_test_to_order: Type[ValueSet], condition_motivating_test: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_LAB_ORDER_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(LabRecommendation(key=key, lab=laboratory_test_to_order, title=title, narrative=description, patient=self.patient, condition=condition_motivating_test))
+
+    def add_procedure(self, title: str, description: str, procedure_to_perform: Type[ValueSet], condition_motivating_procedure: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_PROCEDURE_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(PerformRecommendation(key=key, procedure=procedure_to_perform, condition=condition_motivating_procedure, title=title, narrative=description, patient=self.patient))
+
+    def add_plan_to_create(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_PLAN_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(PlanRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+    def add_prescription(self, title: str, description: str, prescription: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_PRESCRIPTION_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(PrescribeRecommendation(key=key, prescription=prescription, title=title, narrative=description, patient=self.patient))
+
+    def add_referral(self, title: str, description: str, referral: Type[ValueSet], condition_motivating_referral: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_REFERRAL_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(ReferRecommendation(key=key, referral=referral, condition=condition_motivating_referral, title=title, narrative=description, patient=self.patient))
+
+    def add_questionnaire(self, title: str, description: str, structured_questionnaire: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_QUESTIONNAIRE_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(StructuredAssessmentRecommendation(key=key, questionnaires=[structured_questionnaire], title=title, narrative=description, patient=self.patient))
+
+    def add_vital_sign_reading(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_VITAL_SIGN_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(VitalSignRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+class PregnantPersons(ValueSet):
+    VALUE_SET_NAME = 'Pregnant Persons without a known diagnosis of hypertensive disorders of pregnancy or chronic hypertension'
+    SNOMEDCT = {'169826009', '77386006'}
+
+class BloodPressureMeasurements(ValueSet):
+    VALUE_SET_NAME = 'Blood Pressure Measurements'
+    LOINC = {'85354-9', '55284-4'}
+    SNOMEDCT = {'75367002', '271649006'}
+
+class ScreeningFrequencyThroughoutPregnancy(ValueSet):
+    VALUE_SET_NAME = 'Screening Frequency Throughout Pregnancy'
+    SNOMEDCT = {'133931009'}
+
+def recommendations(patient: Patient) -> ComposerResult:
+    result = ComposerResult(patient)
+    title = 'Blood Pressure Screening Recommendation'
+    description = 'It is recommended to perform blood pressure measurements throughout pregnancy to monitor for hypertensive disorders.'
+    result.add_task(title, description)
+    return result
+
+class PregnancyHypertensionScreeningRecommendations(ClinicalQualityMeasure):
+    class Meta:
+        title = "Hypertensive Disorders Pregnancy Screening"
+        description = "USPSTF recommendations on screening pregnant persons for hypertensive disorders"
+        version = "v20240506163929"
+        information = 'https://www.canvasmedical.com/'
+        identifiers = ["AUTO20240506163929"]
+        types = ['CQM']
+        compute_on_change_types = [CHANGE_TYPE.CONDITION,CHANGE_TYPE.LAB_REPORT,CHANGE_TYPE.MEDICATION,CHANGE_TYPE.VITAL_SIGN]
+
+    def in_initial_population(self) -> bool:
+        return in_initial_population(self.patient)
+
+    def in_denominator(self) -> bool:
+        return self.in_initial_population() and in_denominator(self.patient)
+
+    def in_numerator(self) -> bool:
+        return in_numerator(self.patient)
+
+    def compute_results(self) -> ProtocolResult:
+        result = ProtocolResult()
+
+        if self.in_denominator():
+            if self.in_numerator():
+                result.status = STATUS_SATISFIED
+                result.add_narrative(f"{self.patient.first_name} does not need further attention.")
+            else:
+                result.status = STATUS_DUE
+                result.due_in = -1
+                result.add_narrative(f"{self.patient.first_name} does need some attention.")
+                for recommendation in recommendations(self.patient).recommendations:
+                    result.add_recommendation(recommendation)
+        return result
