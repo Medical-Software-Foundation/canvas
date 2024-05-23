3fe99ccb0b09672e20b497c0f69c40476875606d
jubaer145 <mdjubaerhossain7@gmail.com>
2024-05-23 16:54:26 +0600

    Add files for obesity screening for children and adolescents

---
+++ protocols/obesity-in-children-screening/README.md
@@ -0,0 +1,18 @@
+# Screening for Obesity in Children and Adolescents
+
+## Purpose of the protocol
+To update the 2010 US Preventive Services Task Force (USPSTF) recommendation on screening for obesity in children 6 years and older.
+
+## Target population
+- if age >= 6 years and age <= 19 years
+- if BMI >= 95th percentile based on age- and sex-specific CDC growth charts
+
+## Recommendation
+The USPSTF recommends that clinicians screen for obesity in children and adolescents 6 years and older and offer or refer them to comprehensive, intensive behavioral interventions to promote improvements in weight status (B recommendation).
+
+## Importance
+- Prevalence: Approximately 17% of children and adolescents aged 2 to 19 years in the United States have obesity.
+- Health Risks: Associated with mental health and psychological issues, asthma, obstructive sleep apnea, orthopedic problems, and adverse cardiovascular and metabolic outcomes (e.g., high blood pressure, abnormal lipid levels, and insulin resistance).
+- Long-term Impact: Obesity in childhood and adolescence may continue into adulthood, leading to adverse cardiovascular outcomes or other obesity-related morbidity such as type 2 diabetes.
+## Conclusion
+Comprehensive, intensive behavioral interventions (≥26 contact hours) in children and adolescents 6 years and older who have obesity can result in improvements in weight status for up to 12 months. The harms of behavioral interventions are minimal, and the harms of screening are minimal.
---
+++ protocols/obesity-in-children-screening/protocol.py
@@ -0,0 +1,169 @@
+from canvas_workflow_kit.patient import Patient
+import arrow
+from canvas_workflow_kit.value_set import ValueSet
+from typing import Type
+from typing import List
+from canvas_workflow_kit.recommendation import AllergyRecommendation
+from canvas_workflow_kit.recommendation import AssessRecommendation
+from canvas_workflow_kit.recommendation import DiagnoseRecommendation
+from canvas_workflow_kit.recommendation import FollowUpRecommendation
+from canvas_workflow_kit.recommendation import ImagingRecommendation
+from canvas_workflow_kit.recommendation import ImmunizationRecommendation
+from canvas_workflow_kit.recommendation import InstructionRecommendation
+from canvas_workflow_kit.recommendation import InterviewRecommendation
+from canvas_workflow_kit.recommendation import LabRecommendation
+from canvas_workflow_kit.recommendation import PerformRecommendation
+from canvas_workflow_kit.recommendation import PlanRecommendation
+from canvas_workflow_kit.recommendation import PrescribeRecommendation
+from canvas_workflow_kit.recommendation import Recommendation
+from canvas_workflow_kit.recommendation import ReferRecommendation
+from canvas_workflow_kit.recommendation import StructuredAssessmentRecommendation
+from canvas_workflow_kit.recommendation import TaskRecommendation
+from canvas_workflow_kit.recommendation import VitalSignRecommendation
+from canvas_workflow_kit.constants import CHANGE_TYPE
+from canvas_workflow_kit.protocol import ClinicalQualityMeasure
+from canvas_workflow_kit.protocol import STATUS_DUE
+from canvas_workflow_kit.protocol import STATUS_SATISFIED
+from canvas_workflow_kit.protocol import ProtocolResult
+from canvas_workflow_kit.timeframe import Timeframe
+def in_denominator(patient: Patient) -> bool:
+    """
+    Determines if a patient meets the screening criteria of being a child or adolescent 6 years and older.
+    
+    Args:
+    - patient: An instance of the Patient class.
+    
+    Returns:
+    - bool: True if the patient is 6 years or older, False otherwise.
+    """
+    current_date = arrow.now()
+    age = patient.age_at(current_date)
+    return age >= 6
+class ExcludedPatients(ValueSet):
+    VALUE_SET_NAME = 'Excluded Patients'
+
+def in_numerator(patient: Patient) -> bool:
+    return True
+class ComposerResult:
+
+    def __init__(self, patient: Patient) -> None:
+        self.patient = patient
+        self.recommendations: List[Recommendation] = []
+
+    def add_task(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_TASK_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(TaskRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+    def add_allergy_assessment(self, title: str, description: str, potential_allergy: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_ALLERGY_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(AllergyRecommendation(key=key, allergy=potential_allergy, title=title, narrative=description))
+
+    def add_condition_assessment(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_ASSESSMENT_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(AssessRecommendation(key=key, title=title, context={'narrative': description}, patient=self.patient))
+
+    def add_condition_diagnosis(self, title: str, description: str, condition_to_diagnose: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_DIAGNOSIS_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(DiagnoseRecommendation(key=key, condition=condition_to_diagnose, title=title, narrative=description, patient=self.patient))
+
+    def add_follow_up_appointment(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_FOLLOW_UP_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(FollowUpRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+    def add_imaging_order(self, title: str, description: str, imaging_to_order: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_IMAGING_ORDER_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(ImagingRecommendation(key=key, imaging=imaging_to_order, title=title, narrative=description, patient=self.patient))
+
+    def add_immunization_administration(self, title: str, description: str, immunization_to_administer: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_IMMUNIZATION_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(ImmunizationRecommendation(key=key, immunization=immunization_to_administer, title=title, narrative=description, patient=self.patient))
+
+    def add_instruction(self, title: str, description: str, instruction_to_give: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_INSTRUCTION_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(InstructionRecommendation(key=key, instruction=instruction_to_give, title=title, narrative=description, patient=self.patient))
+
+    def add_interview(self, title: str, description: str, questionnaire_to_complete: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_INTERVIEW_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(InterviewRecommendation(key=key, questionnaires=[questionnaire_to_complete], title=title, narrative=description, patient=self.patient))
+
+    def add_lab_order(self, title: str, description: str, laboratory_test_to_order: Type[ValueSet], condition_motivating_test: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_LAB_ORDER_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(LabRecommendation(key=key, lab=laboratory_test_to_order, title=title, narrative=description, patient=self.patient, condition=condition_motivating_test))
+
+    def add_procedure(self, title: str, description: str, procedure_to_perform: Type[ValueSet], condition_motivating_procedure: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_PROCEDURE_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(PerformRecommendation(key=key, procedure=procedure_to_perform, condition=condition_motivating_procedure, title=title, narrative=description, patient=self.patient))
+
+    def add_plan_to_create(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_PLAN_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(PlanRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+    def add_prescription(self, title: str, description: str, prescription: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_PRESCRIPTION_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(PrescribeRecommendation(key=key, prescription=prescription, title=title, narrative=description, patient=self.patient))
+
+    def add_referral(self, title: str, description: str, referral: Type[ValueSet], condition_motivating_referral: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_REFERRAL_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(ReferRecommendation(key=key, referral=referral, condition=condition_motivating_referral, title=title, narrative=description, patient=self.patient))
+
+    def add_questionnaire(self, title: str, description: str, structured_questionnaire: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_QUESTIONNAIRE_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(StructuredAssessmentRecommendation(key=key, questionnaires=[structured_questionnaire], title=title, narrative=description, patient=self.patient))
+
+    def add_vital_sign_reading(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_VITAL_SIGN_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(VitalSignRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+class PatientIdentificationCriteria(ValueSet):
+    VALUE_SET_NAME = 'Children and Adolescents 6 Years and Older at Risk for Obesity'
+    SNOMEDCT = {'90727000', '414915002', '238131007', '230573008', '3542007'}
+    ICD10CM = {'Z68.51', 'Z68.52', 'Z68.53', 'Z68.54'}
+
+class ScreeningMethodBMI(ValueSet):
+    VALUE_SET_NAME = 'BMI Measurement Using Height and Weight'
+    LOINC = {'39156-5', '8302-2', '29463-7', '59576-9', '77606-2'}
+    SNOMEDCT = {'60621009', '248333004'}
+
+class ScreeningFrequency(ValueSet):
+    VALUE_SET_NAME = 'Screening Frequency for Children and Adolescents at Risk for Obesity'
+    SNOMEDCT = {'307681000', '185317003', '410620009'}
+
+def recommendations(patient: Patient) -> ComposerResult:
+    result = ComposerResult(patient)
+    if patient.age_at(time=arrow.now()) >= 6:
+        result.add_task(title='BMI Screening', description='Perform BMI measurement using height and weight as part of routine health maintenance visits. No specific interval recommended.')
+    return result
+
+class ObesityScreeningChildrenUSPSTF(ClinicalQualityMeasure):
+    class Meta:
+        title = "Obesity Screening in Youth"
+        description = "USPSTF recommends obesity screening for children and adolescents 6 years and older"
+        version = "v20240509075545"
+        information = 'https://www.canvasmedical.com/'
+        identifiers = ["AUTO20240509075545"]
+        types = ['CQM']
+        compute_on_change_types = [CHANGE_TYPE.CONDITION,CHANGE_TYPE.VITAL_SIGN,CHANGE_TYPE.LAB_REPORT]
+
+    def in_initial_population(self) -> bool:
+        return in_initial_population(self.patient)
+
+    def in_denominator(self) -> bool:
+        return self.in_initial_population() and in_denominator(self.patient)
+
+    def in_numerator(self) -> bool:
+        return in_numerator(self.patient)
+
+    def compute_results(self) -> ProtocolResult:
+        result = ProtocolResult()
+
+        if self.in_denominator():
+            if self.in_numerator():
+                result.status = STATUS_SATISFIED
+                result.add_narrative(f"{self.patient.first_name} does not need further attention.")
+            else:
+                result.status = STATUS_DUE
+                result.due_in = -1
+                result.add_narrative(f"{self.patient.first_name} does need some attention.")
+                for recommendation in recommendations(self.patient).recommendations:
+                    result.add_recommendation(recommendation)
+        return result
