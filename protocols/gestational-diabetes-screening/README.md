248776b97e33f9852dbe945efe1e8c971d82e422
jubaer145 <mdjubaerhossain7@gmail.com>
2024-05-28 18:31:11 +0600

    Add files for gestational diabetes screening

---
+++ protocols/gestational-diabetes-screening/README.md
@@ -0,0 +1,25 @@
+# Screening for Gestational Diabetes
+## Purpose of the protocol
+To provide recommendations for screening asymptomatic pregnant persons for gestational diabetes to improve maternal and fetal outcomes.
+
+## Target population
+Pregnant persons who have not been previously diagnosed with type 1 or type 2 diabetes.
+Gestational age >= 24 weeks of gestation.
+Asymptomatic pregnant persons.
+If gestational age < 24 weeks of gestation, evidence is insufficient to determine benefits vs. harms of screening.
+
+## Recommendation
+- Screen for gestational diabetes in asymptomatic pregnant persons at 24 weeks of gestation or later.
+- Implement screening using one of several methods, such as:
+    - A 2-step process: Initial screening with an oral glucose challenge test (OGCT), followed by a diagnostic oral glucose tolerance test (OGTT) if initially screened positive.
+    - A 1-step process: Administer an OGTT to all patients.
+    - Fasting plasma glucose measurement.
+## Importance
+- Maternal and fetal complications due to undetected gestational diabetes:
+    - Increased risk of type 2 diabetes post-pregnancy
+    - Maternal risks such as preeclampsia, hypertension
+    - Fetal risks such as macrosomia, birth injuries, and neonatal intensive care unit (NICU) admissions
+- Improves maternal and fetal outcomes through early detection and intervention.
+
+## Conclusion
+The US Preventive Services Task Force (USPSTF) concludes that screening for gestational diabetes at 24 weeks of gestation or after yields a moderate net benefit. Earlier screening is not universally recommended due to insufficient evidence regarding the balance of benefits and harms.
---
+++ protocols/gestational-diabetes-screening/protocol.py
@@ -0,0 +1,259 @@
+from canvas_workflow_kit.value_set import ValueSet
+from canvas_workflow_kit.patient import Patient
+from canvas_workflow_kit.patient_recordset import PatientEventRecordSet
+from canvas_workflow_kit.patient_recordset import PatientPeriodRecordSet
+from typing import Type
+from typing import List
+from canvas_workflow_kit.recommendation import AllergyRecommendation
+from canvas_workflow_kit.recommendation import AssessRecommendation
+from canvas_workflow_kit.recommendation import DiagnoseRecommendation
+from canvas_workflow_kit.recommendation import FollowUpRecommendation
+from canvas_workflow_kit.recommendation import ImagingRecommendation
+from canvas_workflow_kit.recommendation import ImmunizationRecommendation
+from canvas_workflow_kit.recommendation import InstructionRecommendation
+from canvas_workflow_kit.recommendation import InterviewRecommendation
+from canvas_workflow_kit.recommendation import LabRecommendation
+from canvas_workflow_kit.recommendation import PerformRecommendation
+from canvas_workflow_kit.recommendation import PlanRecommendation
+from canvas_workflow_kit.recommendation import PrescribeRecommendation
+from canvas_workflow_kit.recommendation import Recommendation
+from canvas_workflow_kit.recommendation import ReferRecommendation
+from canvas_workflow_kit.recommendation import StructuredAssessmentRecommendation
+from canvas_workflow_kit.recommendation import TaskRecommendation
+from canvas_workflow_kit.recommendation import VitalSignRecommendation
+import arrow
+from canvas_workflow_kit.constants import CHANGE_TYPE
+from canvas_workflow_kit.protocol import ClinicalQualityMeasure
+from canvas_workflow_kit.protocol import STATUS_DUE
+from canvas_workflow_kit.protocol import STATUS_SATISFIED
+from canvas_workflow_kit.protocol import ProtocolResult
+from canvas_workflow_kit.timeframe import Timeframe
+class PregnantPersonsNotPreviouslyDiagnosedWithDiabetes(ValueSet):
+    VALUE_SET_NAME = 'Pregnant persons who have not been previously diagnosed with type 1 or type 2 diabetes'
+    SNOMEDCT = {'169826009', '199227004', '237618001', '77386006', '72892002'}
+    ICD10CM = {'O24.410', 'O24.414', 'O24.419', 'Z34.00', 'Z34.90'}
+
+class AsymptomaticPregnantPersonsAfter24Weeks(ValueSet):
+    VALUE_SET_NAME = 'Asymptomatic pregnant persons at 24 weeks of gestation or after'
+    SNOMEDCT = {'16932000', '714035009', '714036005', '714037001', '714038006'}
+    ICD10CM = {'O09.891', 'O09.892', 'O09.893', 'Z3A.24', 'Z3A.25'}
+
+def in_denominator(patient: Patient) -> bool:
+    return False
+class ComposerResult:
+
+    def __init__(self, patient: Patient) -> None:
+        self.patient = patient
+        self.recommendations: List[Recommendation] = []
+
+    def add_task(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_TASK_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(TaskRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+    def add_allergy_assessment(self, title: str, description: str, potential_allergy: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_ALLERGY_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(AllergyRecommendation(key=key, allergy=potential_allergy, title=title, narrative=description))
+
+    def add_condition_assessment(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_ASSESSMENT_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(AssessRecommendation(key=key, title=title, context={'narrative': description}, patient=self.patient))
+
+    def add_condition_diagnosis(self, title: str, description: str, condition_to_diagnose: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_DIAGNOSIS_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(DiagnoseRecommendation(key=key, condition=condition_to_diagnose, title=title, narrative=description, patient=self.patient))
+
+    def add_follow_up_appointment(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_FOLLOW_UP_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(FollowUpRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+    def add_imaging_order(self, title: str, description: str, imaging_to_order: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_IMAGING_ORDER_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(ImagingRecommendation(key=key, imaging=imaging_to_order, title=title, narrative=description, patient=self.patient))
+
+    def add_immunization_administration(self, title: str, description: str, immunization_to_administer: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_IMMUNIZATION_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(ImmunizationRecommendation(key=key, immunization=immunization_to_administer, title=title, narrative=description, patient=self.patient))
+
+    def add_instruction(self, title: str, description: str, instruction_to_give: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_INSTRUCTION_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(InstructionRecommendation(key=key, instruction=instruction_to_give, title=title, narrative=description, patient=self.patient))
+
+    def add_interview(self, title: str, description: str, questionnaire_to_complete: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_INTERVIEW_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(InterviewRecommendation(key=key, questionnaires=[questionnaire_to_complete], title=title, narrative=description, patient=self.patient))
+
+    def add_lab_order(self, title: str, description: str, laboratory_test_to_order: Type[ValueSet], condition_motivating_test: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_LAB_ORDER_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(LabRecommendation(key=key, lab=laboratory_test_to_order, title=title, narrative=description, patient=self.patient, condition=condition_motivating_test))
+
+    def add_procedure(self, title: str, description: str, procedure_to_perform: Type[ValueSet], condition_motivating_procedure: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_PROCEDURE_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(PerformRecommendation(key=key, procedure=procedure_to_perform, condition=condition_motivating_procedure, title=title, narrative=description, patient=self.patient))
+
+    def add_plan_to_create(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_PLAN_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(PlanRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+    def add_prescription(self, title: str, description: str, prescription: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_PRESCRIPTION_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(PrescribeRecommendation(key=key, prescription=prescription, title=title, narrative=description, patient=self.patient))
+
+    def add_referral(self, title: str, description: str, referral: Type[ValueSet], condition_motivating_referral: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_REFERRAL_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(ReferRecommendation(key=key, referral=referral, condition=condition_motivating_referral, title=title, narrative=description, patient=self.patient))
+
+    def add_questionnaire(self, title: str, description: str, structured_questionnaire: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_QUESTIONNAIRE_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(StructuredAssessmentRecommendation(key=key, questionnaires=[structured_questionnaire], title=title, narrative=description, patient=self.patient))
+
+    def add_vital_sign_reading(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_VITAL_SIGN_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(VitalSignRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+class SevereAllergyValueSet(ValueSet):
+    pass
+
+class TerminalIllnessValueSet(ValueSet):
+    pass
+
+class RecentSurgeryValueSet(ValueSet):
+    pass
+
+def in_numerator(patient: Patient) -> bool:
+    severe_allergies = patient.allergy_intolerances.find(SevereAllergyValueSet)
+    if severe_allergies:
+        return False
+    terminal_illness = patient.conditions.find(TerminalIllnessValueSet)
+    if terminal_illness:
+        return False
+    thirty_days_ago = arrow.now().shift(days=-30)
+    recent_surgery = patient.procedures.after(thirty_days_ago)
+    if recent_surgery:
+        return False
+    return True
+class ComposerResult:
+
+    def __init__(self, patient: Patient) -> None:
+        self.patient = patient
+        self.recommendations: List[Recommendation] = []
+
+    def add_task(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_TASK_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(TaskRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+    def add_allergy_assessment(self, title: str, description: str, potential_allergy: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_ALLERGY_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(AllergyRecommendation(key=key, allergy=potential_allergy, title=title, narrative=description))
+
+    def add_condition_assessment(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_ASSESSMENT_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(AssessRecommendation(key=key, title=title, context={'narrative': description}, patient=self.patient))
+
+    def add_condition_diagnosis(self, title: str, description: str, condition_to_diagnose: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_DIAGNOSIS_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(DiagnoseRecommendation(key=key, condition=condition_to_diagnose, title=title, narrative=description, patient=self.patient))
+
+    def add_follow_up_appointment(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_FOLLOW_UP_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(FollowUpRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+    def add_imaging_order(self, title: str, description: str, imaging_to_order: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_IMAGING_ORDER_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(ImagingRecommendation(key=key, imaging=imaging_to_order, title=title, narrative=description, patient=self.patient))
+
+    def add_immunization_administration(self, title: str, description: str, immunization_to_administer: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_IMMUNIZATION_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(ImmunizationRecommendation(key=key, immunization=immunization_to_administer, title=title, narrative=description, patient=self.patient))
+
+    def add_instruction(self, title: str, description: str, instruction_to_give: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_INSTRUCTION_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(InstructionRecommendation(key=key, instruction=instruction_to_give, title=title, narrative=description, patient=self.patient))
+
+    def add_interview(self, title: str, description: str, questionnaire_to_complete: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_INTERVIEW_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(InterviewRecommendation(key=key, questionnaires=[questionnaire_to_complete], title=title, narrative=description, patient=self.patient))
+
+    def add_lab_order(self, title: str, description: str, laboratory_test_to_order: Type[ValueSet], condition_motivating_test: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_LAB_ORDER_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(LabRecommendation(key=key, lab=laboratory_test_to_order, title=title, narrative=description, patient=self.patient, condition=condition_motivating_test))
+
+    def add_procedure(self, title: str, description: str, procedure_to_perform: Type[ValueSet], condition_motivating_procedure: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_PROCEDURE_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(PerformRecommendation(key=key, procedure=procedure_to_perform, condition=condition_motivating_procedure, title=title, narrative=description, patient=self.patient))
+
+    def add_plan_to_create(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_PLAN_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(PlanRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+    def add_prescription(self, title: str, description: str, prescription: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_PRESCRIPTION_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(PrescribeRecommendation(key=key, prescription=prescription, title=title, narrative=description, patient=self.patient))
+
+    def add_referral(self, title: str, description: str, referral: Type[ValueSet], condition_motivating_referral: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_REFERRAL_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(ReferRecommendation(key=key, referral=referral, condition=condition_motivating_referral, title=title, narrative=description, patient=self.patient))
+
+    def add_questionnaire(self, title: str, description: str, structured_questionnaire: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_QUESTIONNAIRE_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(StructuredAssessmentRecommendation(key=key, questionnaires=[structured_questionnaire], title=title, narrative=description, patient=self.patient))
+
+    def add_vital_sign_reading(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_VITAL_SIGN_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(VitalSignRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+class AsymptomaticPregnantPersons(ValueSet):
+    VALUE_SET_NAME = 'Asymptomatic Pregnant Persons Without Previous Diabetes Diagnosis'
+    SNOMEDCT = {'237630007', '72892002', '77386006'}
+    ICD10CM = {'Z34.00', 'Z34.80', 'Z34.90'}
+
+class ScreeningForGestationalDiabetes(ValueSet):
+    VALUE_SET_NAME = 'Screening for Gestational Diabetes'
+    CPT = {'82947', '82950', '82951'}
+    LOINC = {'1558-6', '1554-5', '14771-0'}
+    ICD10PCS = {'7G31XZZ'}
+
+class ScreeningFrequency(ValueSet):
+    VALUE_SET_NAME = 'Screening Frequency for Gestational Diabetes'
+    SNOMEDCT = {'30766002'}
+
+def recommendations(patient: Patient) -> ComposerResult:
+    result = ComposerResult(patient)
+    if patient.is_female:
+        current_gestational_week = 24
+        if current_gestational_week >= 24:
+            result.add_recommendation(title='Screening for Gestational Diabetes', description='Recommendation to perform screening for gestational diabetes as the patient is at or beyond 24 weeks of gestation.')
+    return result
+
+class GestationalDiabetesScreeningRecommendation(ClinicalQualityMeasure):
+    class Meta:
+        title = "Gestational Diabetes Screening"
+        description = "USPSTF recommendations for gestational diabetes screening in pregnancy"
+        version = "v20240528121021"
+        information = 'https://www.canvasmedical.com/'
+        identifiers = ["AUTO20240528121021"]
+        types = ['CQM']
+        compute_on_change_types = [CHANGE_TYPE.CONDITION,CHANGE_TYPE.LAB_REPORT,CHANGE_TYPE.MEDICATION,CHANGE_TYPE.VITAL_SIGN]
+
+    def in_initial_population(self) -> bool:
+        return in_initial_population(self.patient)
+
+    def in_denominator(self) -> bool:
+        return self.in_initial_population() and in_denominator(self.patient)
+
+    def in_numerator(self) -> bool:
+        return in_numerator(self.patient)
+
+    def compute_results(self) -> ProtocolResult:
+        result = ProtocolResult()
+
+        if self.in_denominator():
+            if self.in_numerator():
+                result.status = STATUS_SATISFIED
+                result.add_narrative(f"{self.patient.first_name} does not need further attention.")
+            else:
+                result.status = STATUS_DUE
+                result.due_in = -1
+                result.add_narrative(f"{self.patient.first_name} does need some attention.")
+                for recommendation in recommendations(self.patient).recommendations:
+                    result.add_recommendation(recommendation)
+        return result
