b7f37d0873815294b2500121ce4421aac984bd5d
jubaer145 <mdjubaerhossain7@gmail.com>
2024-06-06 09:17:42 +0600

    Add files for fracture prevention

---
+++ protocols/fracture-prevention/README.md
@@ -0,0 +1,15 @@
+# Vitamin D and Calcium Supplementation for Fracture Prevention
+## urpose of the Protocol
+The protocol assesses the effectiveness of vitamin D and calcium supplementation in preventing fractures among adults.
+
+## Target Population
+- Adults aged ≥ 50 years (age >= 50)
+- Applies to both genders
+## Recommendation
+- Vitamin D and calcium supplementation is not recommended for the primary prevention of fractures in men and postmenopausal women since the evidence does not clearly demonstrate a substantial benefit.
+## Importance
+- Bone health issues such as osteoporosis are major concerns for adults over 50, leading to increased fracture risks.
+- Fractures can significantly impact quality of life and lead to other serious health complications.
+- Effective prevention strategies are essential to minimize fracture risks and associated health costs.
+## Conclusion
+The use of vitamin D and calcium supplements is not recommended for routine fracture prevention in the general population of adults aged 50 and older. The decision to use these supplements should be personalized based on individual health conditions and risk factors.
---
+++ protocols/fracture-prevention/protocol.py
@@ -0,0 +1,155 @@
+from canvas_workflow_kit.value_set import ValueSet
+from canvas_workflow_kit.patient import Patient
+from typing import Type
+from typing import List
+from canvas_workflow_kit.recommendation import AllergyRecommendation
+from canvas_workflow_kit.recommendation import AssessRecommendation
+from canvas_workflow_kit.recommendation import DiagnoseRecommendation
+from canvas_workflow_kit.recommendation import FollowUpRecommendation
+from canvas_workflow_kit.recommendation import ImagingRecommendation
+from canvas_workflow_kit.recommendation import ImmunizationRecommendation
+from canvas_workflow_kit.recommendation import InstructionRecommendation
+from canvas_workflow_kit.recommendation import InterviewRecommendation
+from canvas_workflow_kit.recommendation import LabRecommendation
+from canvas_workflow_kit.recommendation import PerformRecommendation
+from canvas_workflow_kit.recommendation import PlanRecommendation
+from canvas_workflow_kit.recommendation import PrescribeRecommendation
+from canvas_workflow_kit.recommendation import Recommendation
+from canvas_workflow_kit.recommendation import ReferRecommendation
+from canvas_workflow_kit.recommendation import StructuredAssessmentRecommendation
+from canvas_workflow_kit.recommendation import TaskRecommendation
+from canvas_workflow_kit.recommendation import VitalSignRecommendation
+import arrow
+from canvas_workflow_kit.constants import CHANGE_TYPE
+from canvas_workflow_kit.protocol import ClinicalQualityMeasure
+from canvas_workflow_kit.protocol import STATUS_DUE
+from canvas_workflow_kit.protocol import STATUS_SATISFIED
+from canvas_workflow_kit.protocol import ProtocolResult
+from canvas_workflow_kit.timeframe import Timeframe
+class CommunityDwellingAsymptomaticAdults(ValueSet):
+    VALUE_SET_NAME = 'Community-Dwelling, Asymptomatic Adults'
+    SNOMEDCT = {'160245001', '224960004', '133931000119108'}
+
+class NotLivingInNursingHomeOrOtherInstitutionalCareSetting(ValueSet):
+    VALUE_SET_NAME = 'Not Living in a Nursing Home or Other Institutional Care Setting'
+    SNOMEDCT = {'83891005', '257500003'}
+
+def in_denominator(patient: Patient) -> bool:
+    return False
+class ExcludedPatients(ValueSet):
+    VALUE_SET_NAME = 'ExcludedPatients'
+    ICD10CM = {'Z00.00', 'Z02.5', 'Z02.6', 'Z76.2', 'Z02.89'}
+    SNOMEDCT = {'183932001', '183945008', '183964008', '183949004'}
+
+def in_numerator(patient: Patient) -> bool:
+    return True
+class ComposerResult:
+
+    def __init__(self, patient: Patient) -> None:
+        self.patient = patient
+        self.recommendations: List[Recommendation] = []
+
+    def add_task(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_TASK_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(TaskRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+    def add_allergy_assessment(self, title: str, description: str, potential_allergy: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_ALLERGY_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(AllergyRecommendation(key=key, allergy=potential_allergy, title=title, narrative=description))
+
+    def add_condition_assessment(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_ASSESSMENT_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(AssessRecommendation(key=key, title=title, context={'narrative': description}, patient=self.patient))
+
+    def add_condition_diagnosis(self, title: str, description: str, condition_to_diagnose: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_DIAGNOSIS_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(DiagnoseRecommendation(key=key, condition=condition_to_diagnose, title=title, narrative=description, patient=self.patient))
+
+    def add_follow_up_appointment(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_FOLLOW_UP_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(FollowUpRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+    def add_imaging_order(self, title: str, description: str, imaging_to_order: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_IMAGING_ORDER_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(ImagingRecommendation(key=key, imaging=imaging_to_order, title=title, narrative=description, patient=self.patient))
+
+    def add_immunization_administration(self, title: str, description: str, immunization_to_administer: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_IMMUNIZATION_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(ImmunizationRecommendation(key=key, immunization=immunization_to_administer, title=title, narrative=description, patient=self.patient))
+
+    def add_instruction(self, title: str, description: str, instruction_to_give: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_INSTRUCTION_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(InstructionRecommendation(key=key, instruction=instruction_to_give, title=title, narrative=description, patient=self.patient))
+
+    def add_interview(self, title: str, description: str, questionnaire_to_complete: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_INTERVIEW_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(InterviewRecommendation(key=key, questionnaires=[questionnaire_to_complete], title=title, narrative=description, patient=self.patient))
+
+    def add_lab_order(self, title: str, description: str, laboratory_test_to_order: Type[ValueSet], condition_motivating_test: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_LAB_ORDER_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(LabRecommendation(key=key, lab=laboratory_test_to_order, title=title, narrative=description, patient=self.patient, condition=condition_motivating_test))
+
+    def add_procedure(self, title: str, description: str, procedure_to_perform: Type[ValueSet], condition_motivating_procedure: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_PROCEDURE_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(PerformRecommendation(key=key, procedure=procedure_to_perform, condition=condition_motivating_procedure, title=title, narrative=description, patient=self.patient))
+
+    def add_plan_to_create(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_PLAN_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(PlanRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+    def add_prescription(self, title: str, description: str, prescription: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_PRESCRIPTION_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(PrescribeRecommendation(key=key, prescription=prescription, title=title, narrative=description, patient=self.patient))
+
+    def add_referral(self, title: str, description: str, referral: Type[ValueSet], condition_motivating_referral: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_REFERRAL_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(ReferRecommendation(key=key, referral=referral, condition=condition_motivating_referral, title=title, narrative=description, patient=self.patient))
+
+    def add_questionnaire(self, title: str, description: str, structured_questionnaire: Type[ValueSet]) -> None:
+        key = f'RECOMMEND_QUESTIONNAIRE_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(StructuredAssessmentRecommendation(key=key, questionnaires=[structured_questionnaire], title=title, narrative=description, patient=self.patient))
+
+    def add_vital_sign_reading(self, title: str, description: str) -> None:
+        key = f'RECOMMEND_VITAL_SIGN_{1 + len(self.recommendations):02d}'
+        self.recommendations.append(VitalSignRecommendation(key=key, title=title, narrative=description, patient=self.patient))
+
+def recommendations(patient: Patient) -> ComposerResult:
+    result = ComposerResult(patient)
+    result.add_task(title='Check Flu Vaccination Status', description="Review patient's immunization records to ensure annual flu vaccination is up to date.")
+    result.add_task(title='Monitor Blood Pressure', description='For patients with a history of hypertension, ensure regular monitoring of blood pressure.')
+    result.add_task(title='Review Diabetes Management Plan', description='For diabetic patients, review and update the diabetes management plan as necessary.')
+    return result
+
+class FracturePreventionGuidelinesUSPSTF(ClinicalQualityMeasure):
+    class Meta:
+        title = "Fracture Prevention Guidelines"
+        description = "USPSTFs guidelines on vitamin D and calcium supplementation for fracture prevention in adults"
+        version = "v20240606031042"
+        information = 'https://www.canvasmedical.com/'
+        identifiers = ["AUTO20240606031042"]
+        types = ['CQM']
+        compute_on_change_types = []
+
+    def in_initial_population(self) -> bool:
+        return in_initial_population(self.patient)
+
+    def in_denominator(self) -> bool:
+        return self.in_initial_population() and in_denominator(self.patient)
+
+    def in_numerator(self) -> bool:
+        return in_numerator(self.patient)
+
+    def compute_results(self) -> ProtocolResult:
+        result = ProtocolResult()
+
+        if self.in_denominator():
+            if self.in_numerator():
+                result.status = STATUS_SATISFIED
+                result.add_narrative(f"{self.patient.first_name} does not need further attention.")
+            else:
+                result.status = STATUS_DUE
+                result.due_in = -1
+                result.add_narrative(f"{self.patient.first_name} does need some attention.")
+                for recommendation in recommendations(self.patient).recommendations:
+                    result.add_recommendation(recommendation)
+        return result
