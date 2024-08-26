702949dc620105c98670183fb90e084656231409
Kristen ONeill <91080969+kristenoneill@users.noreply.github.com>
2024-08-26 10:38:41 -0500

    appointment-reschedule

---
+++ protocols/appointment-reschedule/appointment-reschedule.py
@@ -0,0 +1,188 @@
+from enum import Enum
+from typing import List
+
+from canvas_workflow_kit.patient_recordset import (
+    InterviewRecordSet,
+    TaskRecordSet,
+)
+from canvas_workflow_kit.protocol import (
+    CHANGE_TYPE,
+    STATUS_DUE,
+    STATUS_NOT_APPLICABLE,
+    STATUS_SATISFIED,
+    ClinicalQualityMeasure,
+    ProtocolResult,
+)
+from canvas_workflow_kit.value_set import ValueSet
+
+# Replace with the ID of your phone call disposition questionnaire
+PHONE_CALL_DISPOSITION_QUESTIONNAIRE_ID = 'QUES_PHONE_01'
+# Replace with your task type
+APPOINTMENT_RESCHEDULE_TASK_TYPE = 'Appointment Reschedule'
+
+
+class PhoneCallDispositionQuestionnaire(ValueSet):
+    VALUE_SET_NAME = 'Phone Call Disposition Questionnaire'
+    INTERNAL = {PHONE_CALL_DISPOSITION_QUESTIONNAIRE_ID}
+
+
+class PhoneResponses(Enum):
+    REACHED = 'QUES_PHONE_03'
+    REACHED_NOT_INTERESTED = 'QUES_PHONE_04'
+    NO_ANSWER_MESSAGE = 'QUES_PHONE_05'
+    NO_ANSWER_NO_MESSAGE = 'QUES_PHONE_06'
+    CALL_BACK_REQUESTED = 'QUES_PHONE_08'
+    CALL_TO_PATIENT = 'QUES_PHONE_10'
+    CALL_TO_OTHER = 'QUES_PHONE_11'
+    FREE_TEXT = 'QUES_PHONE_16'
+
+
+class AnnualAssessment(ValueSet):
+    VALUE_SET_NAME = 'Annual Visit'
+    CPT = {
+        '99201',
+        '99202',
+        '99203',
+        '99204',
+        '99205',
+        '99214',
+        '99215',
+        '99341',
+        '99342',
+        '99344',
+        '99345',
+        '99347',
+        '99348',
+        '99349',
+        '99350',
+        '99441',
+        '99442',
+        '99443',
+    }
+
+
+class AppointmentReschedule(ClinicalQualityMeasure):
+    class Meta:
+        title = 'Appointments: Rescheduled'
+        description = ()
+        version = '1.0.0'
+        information = 'https://canvasmedical.com/gallery'
+        identifiers: List[str] = []
+        types = ['DUO']
+        compute_on_change_types = [
+            CHANGE_TYPE.INTERVIEW,
+            CHANGE_TYPE.APPOINTMENT,
+            CHANGE_TYPE.TASK,
+        ]
+        references: List[str] = []
+
+    def _get_phone_calls_to_patient(self) -> InterviewRecordSet:
+        '''Get all phone calls made to this patient.
+
+        Returns:
+            InterviewRecordSet: A set of phone call records made to the patient.
+        '''
+        phone_calls = self.patient.interviews.find(PhoneCallDispositionQuestionnaire).filter(
+            status='AC'
+        )
+        return InterviewRecordSet(
+            [
+                phone_call
+                for phone_call in phone_calls
+                if any(
+                    PhoneResponses(response['code']) == PhoneResponses.CALL_TO_PATIENT
+                    for response in phone_call['responses']
+                )
+            ]
+        )
+
+    def _get_phone_call_responses(self) -> List[PhoneResponses]:
+        '''Get all phone call responses for this patient.
+
+        Returns:
+            A list of PhoneResponses objects representing the phone call responses.
+        '''
+        return [
+            PhoneResponses(response['code'])
+            for phone_call in self._get_phone_calls_to_patient().records
+            for response in phone_call['responses']
+        ]
+
+    def _get_tasks_by_label(self, labels: List[str]) -> TaskRecordSet:
+        '''
+        Get all tasks with specified labels.
+
+        Args:
+            labels (List[str]): The list of labels to filter tasks by.
+
+        Returns:
+            TaskRecordSet: A TaskRecordSet object containing the filtered tasks.
+        '''
+        open_tasks = self.patient.tasks.filter(
+            status='OPEN',
+        )
+        return TaskRecordSet(
+            [task for task in open_tasks if any(label in labels for label in task['labels'])]
+        )
+
+    def in_denominator(self) -> bool:
+        '''
+        Check for patients with an "appointment reschedule" task.
+
+        Returns:
+            bool: True if the patient has an "appointment reschedule" task, False otherwise.
+        '''
+        return bool(self._get_tasks_by_label([APPOINTMENT_RESCHEDULE_TASK_TYPE]))
+
+    def in_numerator(self) -> bool:
+        '''
+        Check for patients who have had either:
+            - 1 "patient reached"
+            - 2 "phone call attempts".
+
+        Returns:
+            bool: True if the patient satisfies the above condition False otherwise.
+        '''
+        responses = self._get_phone_call_responses()
+        attempts = len(
+            [
+                response
+                for response in responses
+                if response
+                in [
+                    PhoneResponses.NO_ANSWER_MESSAGE,
+                    PhoneResponses.NO_ANSWER_NO_MESSAGE,
+                ]
+            ]
+        )
+        reached = any(response for response in responses if response == PhoneResponses.REACHED)
+        return reached or attempts >= 2
+
+    def compute_results(self) -> ProtocolResult:
+        '''
+        Computes the results of the appointment reschedule protocol.
+
+        Returns:
+            ProtocolResult: The computed results of the protocol.
+        '''
+        result = ProtocolResult()
+        if self.in_denominator():
+            if self.in_numerator():
+                result.status = STATUS_SATISFIED
+                result.add_narrative(
+                    'Patient has been contacted or has attempted to be contacted. No action needed.'
+                )
+            else:
+                result.due_in = -1
+                result.status = STATUS_DUE
+                result.add_narrative(
+                    (
+                        'Patient has a task to reschedule their appointment. '
+                        'Contact them to reschedule their appointment.'
+                    )
+                )
+        else:
+            result.status = STATUS_NOT_APPLICABLE
+            result.add_narrative('Patient does not have an appointment to reschedule.')
+
+        return result
