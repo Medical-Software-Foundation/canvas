628c4d0502b6c3e1fa5d97ce0273bee5c7847a6d
Reba Magier <reba.magier@canvasmedical.com>
2025-02-07 18:52:08 -0500

    add create patients for athena migration

--- data-migrations/data_migrations/template_migration/utils.py
+++ data-migrations/data_migrations/template_migration/utils.py
@@ -41,7 +41,7 @@ def validate_state_code(value, field_name):
     if not value:
         return True, value
 
-    accepted_states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+    accepted_states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
     if value in accepted_states:
         return True, value
     return False, f"Invalid {field_name}: {value}"
