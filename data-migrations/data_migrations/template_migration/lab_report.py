c02216f6f1e8ea762aa2a0523fa8143a02d1adfc
Reba Magier <reba.magier@canvasmedical.com>
2025-06-25 13:57:51 -0400

    save patient id for done labs

--- data-migrations/data_migrations/template_migration/lab_report.py
+++ data-migrations/data_migrations/template_migration/lab_report.py
@@ -14,7 +14,7 @@ class LabReportMixin(MappingMixin, FileWriterMixin, DocumentEncoderMixin):
                 continue
             try:
                 canvas_id = self.fumage_helper.perform_create_lab_report(payload_dict["payload"])
-                self.done_row(f"{payload_dict['unique_attribute']}|{canvas_id}")
+                self.done_row(f"{payload_dict['unique_attribute']}|{payload_dict['patient_id']}|{payload_dict['canvas_patient_key']}|{canvas_id}")
                 ids.add(payload_dict['unique_attribute'])
             except BaseException as e:
                 self.error_row(f"{payload_dict['unique_attribute']}|{payload_dict['patient_id']}|{payload_dict['canvas_patient_key']}", f"{str(e)}")
