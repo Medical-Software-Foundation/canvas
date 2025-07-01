a20b753b3063e4a536986dbfacad26650a9237e6
Joe Wilson <joe.wilson@canvasmedical.com>
2025-07-01 09:06:35 -0700

    Adds progress counter to lab reports import.

--- data-migrations/data_migrations/template_migration/lab_report.py
+++ data-migrations/data_migrations/template_migration/lab_report.py
@@ -8,9 +8,13 @@ from data_migrations.template_migration.utils import (
 class LabReportMixin(MappingMixin, FileWriterMixin, DocumentEncoderMixin):
     def load(self, validated_rows):
         ids = set()
+        record_count = len(validated_rows)
+        record_num = 1
         for payload_dict in validated_rows:
+            print(f"Importing {record_num} of {record_count} lab reports.")
             if payload_dict["unique_attribute"] in self.done_records or payload_dict["unique_attribute"] in ids:
                 print(' Already did record')
+                record_num += 1
                 continue
             try:
                 canvas_id = self.fumage_helper.perform_create_lab_report(payload_dict["payload"])
@@ -18,3 +22,5 @@ class LabReportMixin(MappingMixin, FileWriterMixin, DocumentEncoderMixin):
                 ids.add(payload_dict['unique_attribute'])
             except BaseException as e:
                 self.error_row(f"{payload_dict['unique_attribute']}|{payload_dict['patient_id']}|{payload_dict['canvas_patient_key']}", f"{str(e)}")
+
+            record_num += 1
