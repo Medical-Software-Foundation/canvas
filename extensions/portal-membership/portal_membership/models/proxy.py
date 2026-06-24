c5194dfd00e2f46889e968f3779dc2579429e560
Kristen ONeill <91080969+kristenoneill@users.noreply.github.com>
2026-06-23 20:22:01 -0500

    Delete extensions/portal-membership directory

--- extensions/portal-membership/portal_membership/models/proxy.py
+++
@@ -1,7 +0,0 @@
-from canvas_sdk.v1.data import ModelExtension, Patient
-
-
-class PatientProxy(Patient, ModelExtension):
-    """Proxy model so CustomModels can declare a ForeignKey to Patient."""
-
-    pass
